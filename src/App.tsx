52524582daa2f1cabd4a08641353196dc6d33fc0
Igor Ribeiro <igor.piresmeira@gmail.com>
2022-06-24 23:57:41 -0300

    Projeto REACT Ignite Lab utilizando GraphCMS

---
+++ src/App.tsx
@@ -0,0 +1,26 @@
+import { ApolloProvider } from "@apollo/client";
+import { BrowserRouter } from "react-router-dom";
+import { Header } from "./components/Header";
+import { Lesson } from "./components/Lesson";
+import { Sidebar } from "./components/Sidebar";
+import { Video } from "./components/Video";
+import { client } from "./lib/apollo";
+import { Evento } from "./pages/Evento";
+import { Router } from "./Router";
+
+
+
+function App() {
+
+  return (
+    <div>
+      <ApolloProvider client={client}>
+        <BrowserRouter>
+          <Router />
+        </BrowserRouter>
+      </ApolloProvider>
+    </div>
+  )
+}
+
+export default App;
